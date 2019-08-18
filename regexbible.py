1ed01a506a6cd40ba9e77799a4c54eac85e2ebfb
Logan Williams <loganwilliams116@gmail.com>
2019-08-17 18:00:46 -0700

    Improve error results, left-justify results

--- regexbible.py
+++ regexbible.py
@@ -4,6 +4,8 @@ from parse_json_bible import regex_search_bibles
 
 app = Flask(__name__)
 
+ERROR_TEMPLATE = "search_results_error.html"
+RESULTS_TEMPLATE = "search_results.hml"
 
 @app.route("/")
 def index():
@@ -14,12 +16,15 @@ def search():
     regex = request.args.get('regex')
     translations_to_search = json.loads(request.args.get('versions'))
 
+    if len(translations_to_search) == 0:
+        return render_template(ERROR_TEMPLATE, error="Please select a translation to search.")
+    elif regex.strip() == "":
+        return render_template(ERROR_TEMPLATE, error="Please enter something to search.")
+
     # get regex results
     results = regex_search_bibles(translations_to_search, regex)
 
-    if len(translations_to_search) == 0:
-    	return "<div>Please select a translation to search.</div>"
-    elif results:
-        return render_template('search_results.html', search_results=results, result_count=len(results))
+    if results:
+        return render_template(RESULTS_TEMPLATE, search_results=results, result_count=len(results))
     else:
-        return "<div>Your search did not match any verses in the translations " + ", ".join(translations_to_search) + ".</div>"
+        return render_template(ERROR_TEMPLATE, error="Your search did not match any verses in the translations " + ", ".join(translations_to_search) + ".")
